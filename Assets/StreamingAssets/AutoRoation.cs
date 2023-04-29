93622caa5674dca5d2d706b2cbfb79fdd6a572eb
sswati0390 <s.swati0390@gmail.com>
2023-04-29 12:56:56 +0530

    Changes added for Prop1

---
+++ Assets/StreamingAssets/AutoRoation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRoation : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(0, 0.5f, 0);
+    }
+
+}
